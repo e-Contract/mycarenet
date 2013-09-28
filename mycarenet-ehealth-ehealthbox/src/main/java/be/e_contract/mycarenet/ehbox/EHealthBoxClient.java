3be4372148123206fb9239182f52bf20c8c537bb
Frank Cornelis <info@e-contract.be>
2013-09-28 12:16:41 +0200

    eHealthBox invoke DOM interface

--- mycarenet-ehealth-ehealthbox/src/main/java/be/e_contract/mycarenet/ehbox/EHealthBoxClient.java
+++ mycarenet-ehealth-ehealthbox/src/main/java/be/e_contract/mycarenet/ehbox/EHealthBoxClient.java
@@ -21,8 +21,15 @@ package be.e_contract.mycarenet.ehbox;
 import java.security.PrivateKey;
 import java.util.List;
 
+import javax.xml.namespace.QName;
+import javax.xml.transform.Source;
+import javax.xml.transform.dom.DOMSource;
 import javax.xml.ws.Binding;
 import javax.xml.ws.BindingProvider;
+import javax.xml.ws.Dispatch;
+import javax.xml.ws.Service;
+
+import org.w3c.dom.Element;
 
 import be.e_contract.mycarenet.common.LoggingHandler;
 import be.e_contract.mycarenet.ehbox.jaxb.consultation.protocol.GetBoxInfoRequestType;
@@ -36,6 +43,8 @@ public class EHealthBoxClient {
 
 	private final EhBoxConsultationPortType ehBoxConsultationPort;
 
+	private final Dispatch<Source> dispatch;
+
 	private final WSSecuritySOAPHandler wsSecuritySOAPHandler;
 
 	public EHealthBoxClient(String location) throws Exception {
@@ -43,13 +52,25 @@ public class EHealthBoxClient {
 				.newInstance();
 		this.ehBoxConsultationPort = service.getEhBoxConsultationPort();
 
-		BindingProvider bindingProvider = (BindingProvider) this.ehBoxConsultationPort;
+		QName portQName = new QName(
+				"urn:be:fgov:ehealth:ehbox:consultation:protocol:v2",
+				"ehBoxConsultationPort");
+		this.dispatch = service.createDispatch(portQName, Source.class,
+				Service.Mode.PAYLOAD);
+
+		this.wsSecuritySOAPHandler = new WSSecuritySOAPHandler();
+		configureBindingProvider((BindingProvider) this.ehBoxConsultationPort,
+				location);
+		configureBindingProvider(this.dispatch, location);
+	}
+
+	private void configureBindingProvider(BindingProvider bindingProvider,
+			String location) {
 		bindingProvider.getRequestContext().put(
 				BindingProvider.ENDPOINT_ADDRESS_PROPERTY, location);
 
 		Binding binding = bindingProvider.getBinding();
 		List handlerChain = binding.getHandlerChain();
-		this.wsSecuritySOAPHandler = new WSSecuritySOAPHandler();
 		handlerChain.add(this.wsSecuritySOAPHandler);
 		handlerChain.add(new LoggingHandler());
 		binding.setHandlerChain(handlerChain);
@@ -63,5 +84,15 @@ public class EHealthBoxClient {
 		GetBoxInfoResponseType getBoxInfoResponse = this.ehBoxConsultationPort
 				.getBoxInfo(getBoxInfoRequest);
 		return getBoxInfoResponse;
+	}
+
+	public Element invoke(Element request, PrivateKey hokPrivateKey,
+			String samlAssertion) {
+		this.wsSecuritySOAPHandler.setPrivateKey(hokPrivateKey);
+		this.wsSecuritySOAPHandler.setAssertion(samlAssertion);
+		Source responseSource = this.dispatch.invoke(new DOMSource(request));
+		DOMSource responseDOMSource = (DOMSource) responseSource;
+		Element responseElement = (Element) responseDOMSource.getNode();
+		return responseElement;
 	}
 }
