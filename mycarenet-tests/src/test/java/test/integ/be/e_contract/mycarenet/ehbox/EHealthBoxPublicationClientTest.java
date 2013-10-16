b3698167789c29fdc318c5d7f973163ab56373d0
Frank Cornelis <info@e-contract.be>
2013-10-16 07:04:26 +0200

    integration test for eHealthBox Publication with Annex

--- mycarenet-tests/src/test/java/test/integ/be/e_contract/mycarenet/ehbox/EHealthBoxPublicationClientTest.java
+++ mycarenet-tests/src/test/java/test/integ/be/e_contract/mycarenet/ehbox/EHealthBoxPublicationClientTest.java
@@ -49,6 +49,7 @@ import be.e_contract.mycarenet.ehbox.jaxb.publication.protocol.ContentContextTyp
 import be.e_contract.mycarenet.ehbox.jaxb.publication.protocol.ContentSpecificationType;
 import be.e_contract.mycarenet.ehbox.jaxb.publication.protocol.DestinationContextType;
 import be.e_contract.mycarenet.ehbox.jaxb.publication.protocol.ObjectFactory;
+import be.e_contract.mycarenet.ehbox.jaxb.publication.protocol.PublicationAnnexType;
 import be.e_contract.mycarenet.ehbox.jaxb.publication.protocol.PublicationContentType;
 import be.e_contract.mycarenet.ehbox.jaxb.publication.protocol.PublicationDocumentType;
 import be.e_contract.mycarenet.ehbox.jaxb.publication.protocol.PublicationMessageType;
@@ -160,6 +161,116 @@ public class EHealthBoxPublicationClientTest {
 
 		ContentSpecificationType contentSpecification = objectFactory
 				.createContentSpecificationType();
+		contentContext.setContentSpecification(contentSpecification);
+		contentSpecification.setContentType("DOCUMENT");
+
+		publicationClient.publish(publicationMessage, eHealthPrivateKey,
+				assertionString);
+	}
+
+	@Test
+	public void testPublishAnnex() throws Exception {
+		// STS
+		EHealthSTSClient client = new EHealthSTSClient(
+				"https://wwwacc.ehealth.fgov.be/sts_1_1/SecureTokenService");
+
+		Security.addProvider(new BeIDProvider());
+		KeyStore keyStore = KeyStore.getInstance("BeID");
+		keyStore.load(null);
+
+		PrivateKey authnPrivateKey = (PrivateKey) keyStore.getKey(
+				"Authentication", null);
+		X509Certificate authnCertificate = (X509Certificate) keyStore
+				.getCertificate("Authentication");
+
+		KeyStore eHealthKeyStore = KeyStore.getInstance("PKCS12");
+		FileInputStream fileInputStream = new FileInputStream(
+				this.config.getEHealthPKCS12Path());
+		eHealthKeyStore.load(fileInputStream, this.config
+				.getEHealthPKCS12Password().toCharArray());
+		Enumeration<String> aliasesEnum = eHealthKeyStore.aliases();
+		String alias = aliasesEnum.nextElement();
+		X509Certificate eHealthCertificate = (X509Certificate) eHealthKeyStore
+				.getCertificate(alias);
+		PrivateKey eHealthPrivateKey = (PrivateKey) eHealthKeyStore.getKey(
+				alias, this.config.getEHealthPKCS12Password().toCharArray());
+
+		List<Attribute> attributes = new LinkedList<Attribute>();
+		attributes.add(new Attribute("urn:be:fgov:identification-namespace",
+				"urn:be:fgov:ehealth:1.0:certificateholder:person:ssin"));
+		attributes.add(new Attribute("urn:be:fgov:identification-namespace",
+				"urn:be:fgov:person:ssin"));
+
+		List<AttributeDesignator> attributeDesignators = new LinkedList<AttributeDesignator>();
+		attributeDesignators.add(new AttributeDesignator(
+				"urn:be:fgov:identification-namespace",
+				"urn:be:fgov:ehealth:1.0:certificateholder:person:ssin"));
+		attributeDesignators.add(new AttributeDesignator(
+				"urn:be:fgov:identification-namespace",
+				"urn:be:fgov:person:ssin"));
+		attributeDesignators.add(new AttributeDesignator(
+				"urn:be:fgov:certified-namespace:ehealth",
+				"urn:be:fgov:person:ssin:nurse:boolean"));
+
+		Element assertion = client.requestAssertion(authnCertificate,
+				authnPrivateKey, eHealthCertificate, eHealthPrivateKey,
+				attributes, attributeDesignators);
+
+		assertNotNull(assertion);
+
+		String assertionString = client.toString(assertion);
+
+		// eHealthBox publication
+		EHealthBoxPublicationClient publicationClient = new EHealthBoxPublicationClient(
+				"https://services-acpt.ehealth.fgov.be/ehBoxPublication/v3");
+
+		ObjectFactory objectFactory = new ObjectFactory();
+		PublicationMessageType publicationMessage = objectFactory
+				.createPublicationMessageType();
+		String publicationId = UUID.randomUUID().toString().substring(1, 13);
+		LOG.debug("publication id: " + publicationId);
+		publicationMessage.setPublicationId(publicationId);
+
+		DestinationContextType destinationContext = objectFactory
+				.createDestinationContextType();
+		publicationMessage.getDestinationContext().add(destinationContext);
+		destinationContext.setQuality("NURSE");
+		destinationContext.setType("INSS");
+		destinationContext.setId(getUserIdentifier(authnCertificate));
+
+		ContentContextType contentContext = objectFactory
+				.createContentContextType();
+		publicationMessage.setContentContext(contentContext);
+
+		PublicationContentType publicationContent = objectFactory
+				.createPublicationContentType();
+		contentContext.setContent(publicationContent);
+		PublicationDocumentType publicationDocument = objectFactory
+				.createPublicationDocumentType();
+		publicationContent.setDocument(publicationDocument);
+		publicationDocument.setTitle("test");
+		publicationDocument.setMimeType("text/plain");
+		publicationDocument.setDownloadFileName("test.txt");
+		byte[] message = "hello world".getBytes();
+		publicationDocument.setEncryptableTextContent(message);
+		MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+		byte[] digest = messageDigest.digest(message);
+		publicationDocument.setDigest(Base64.encodeBase64String(digest));
+
+		List<PublicationAnnexType> publicationAnnexList = publicationContent
+				.getAnnex();
+		PublicationAnnexType publicationAnnex = objectFactory
+				.createPublicationAnnexType();
+		publicationAnnexList.add(publicationAnnex);
+		publicationAnnex.setDownloadFileName("test.txt");
+		publicationAnnex.setEncryptableTitle("hello world".getBytes());
+		publicationAnnex.setMimeType("application/octet-stream");
+		publicationAnnex.setEncryptableTextContent(message);
+		publicationAnnex.setDigest(Base64.encodeBase64String(digest));
+
+		ContentSpecificationType contentSpecification = objectFactory
+				.createContentSpecificationType();
+
 		contentContext.setContentSpecification(contentSpecification);
 		contentSpecification.setContentType("DOCUMENT");
 
