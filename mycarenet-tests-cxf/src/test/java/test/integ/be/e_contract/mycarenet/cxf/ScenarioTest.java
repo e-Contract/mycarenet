787c5b2286a7c3d534fe1a50fa08cbb486d5e2eb
Frank Cornelis <info@e-contract.be>
2014-02-14 10:38:27 +0100

    fix to disable Apache CXF MTOM

--- mycarenet-tests-cxf/src/test/java/test/integ/be/e_contract/mycarenet/cxf/ScenarioTest.java
+++ mycarenet-tests-cxf/src/test/java/test/integ/be/e_contract/mycarenet/cxf/ScenarioTest.java
@@ -182,7 +182,8 @@ public class ScenarioTest {
 				.createPublicationDocumentType();
 		publicationContent.setDocument(publicationDocument);
 		publicationDocument.setTitle("test");
-		publicationDocument.setMimeType("application/octet-stream");
+		publicationDocument.setMimeType("text/plain");
+		// publicationDocument.setMimeType("application/octet-stream");
 		publicationDocument.setDownloadFileName("test.dat");
 		byte[] data = new byte[1024 * 256];
 		DataSource dataSource = new ByteArrayDataSource(data,
@@ -200,7 +201,7 @@ public class ScenarioTest {
 
 		publicationClient.setCredentials(eHealthPrivateKey, assertionString);
 		publicationClient.publish(publicationMessage);
-		
+
 		Thread.sleep(1000 * 5);
 
 		LOG.debug("GET MESSAGES LIST");
@@ -360,7 +361,7 @@ public class ScenarioTest {
 
 		publicationClient.setCredentials(eHealthPrivateKey, assertionString);
 		publicationClient.publish(publicationMessage);
-		
+
 		// give eHealthBox some time.
 		Thread.sleep(1000 * 5);
 
@@ -383,13 +384,205 @@ public class ScenarioTest {
 					.createUnmarshaller();
 			Map<String, DataHandler> messageAttachments = eHealthBoxClient
 					.getMessageAttachments();
-			for (Map.Entry<String, DataHandler> messageAttachment : messageAttachments.entrySet()) {
-				LOG.debug("message attachment id: " + messageAttachment.getKey());
-				LOG.debug("message data handler: " + messageAttachment.getValue());
+			for (Map.Entry<String, DataHandler> messageAttachment : messageAttachments
+					.entrySet()) {
+				LOG.debug("message attachment id: "
+						+ messageAttachment.getKey());
+				LOG.debug("message data handler: "
+						+ messageAttachment.getValue());
 				DataHandler resultDataHandler = messageAttachment.getValue();
 				DataSource resultDataSource = resultDataHandler.getDataSource();
-				byte[] attachmentData = IOUtils.toByteArray(resultDataSource.getInputStream());
-				LOG.debug("DataHandler.DataSource.getInputStream length: " + attachmentData.length);
+				byte[] attachmentData = IOUtils.toByteArray(resultDataSource
+						.getInputStream());
+				LOG.debug("DataHandler.DataSource.getInputStream length: "
+						+ attachmentData.length);
+			}
+			consultationUnmarshaller
+					.setAttachmentUnmarshaller(new SOAPAttachmentUnmarshaller(
+							messageAttachments));
+			JAXBElement<GetFullMessageResponseType> jaxbElement = (JAXBElement<GetFullMessageResponseType>) consultationUnmarshaller
+					.unmarshal(new StringReader(response));
+			GetFullMessageResponseType getFullMessageResponse = jaxbElement
+					.getValue();
+			ConsultationMessageType consultationMessage = getFullMessageResponse
+					.getMessage();
+			be.e_contract.mycarenet.ehbox.jaxb.consultation.protocol.ContentContextType consultationContentContext = consultationMessage
+					.getContentContext();
+			ConsultationContentType consultationContent = consultationContentContext
+					.getContent();
+			ConsultationDocumentType consultationDocument = consultationContent
+					.getDocument();
+			byte[] encryptableTextContent = consultationDocument
+					.getEncryptableTextContent();
+			if (null != encryptableTextContent) {
+				LOG.debug("result EncryptableTextContent: "
+						+ encryptableTextContent.length);
+			} else {
+				LOG.debug("no EncryptableTextContent");
+			}
+			DataHandler resultDataHandler = consultationDocument
+					.getEncryptableBinaryContent();
+			if (null != resultDataHandler) {
+				LOG.debug("result EncryptableBinaryContent");
+				byte[] resultData = IOUtils.toByteArray(resultDataHandler
+						.getInputStream());
+				LOG.debug("result data size: " + resultData.length);
+			}
+			LOG.debug("DELETE MESSAGE");
+			eHealthBoxClient.deleteMessage(messageId);
+		}
+	}
+
+	/**
+	 * First we clean the eHealthBox. Then we publish to ourself. Next we
+	 * download this message.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testScenarioInvokePlainText() throws Exception {
+		// STS
+		EHealthSTSClient client = new EHealthSTSClient(
+				"https://wwwacc.ehealth.fgov.be/sts_1_1/SecureTokenService");
+
+		Security.addProvider(new BeIDProvider());
+		KeyStore keyStore = KeyStore.getInstance("BeID");
+		keyStore.load(null);
+		PrivateKey authnPrivateKey = (PrivateKey) keyStore.getKey(
+				"Authentication", null);
+		X509Certificate authnCertificate = (X509Certificate) keyStore
+				.getCertificate("Authentication");
+
+		KeyStore eHealthKeyStore = KeyStore.getInstance("PKCS12");
+		FileInputStream fileInputStream = new FileInputStream(
+				this.config.getEHealthPKCS12Path());
+		eHealthKeyStore.load(fileInputStream, this.config
+				.getEHealthPKCS12Password().toCharArray());
+		Enumeration<String> aliasesEnum = eHealthKeyStore.aliases();
+		String alias = aliasesEnum.nextElement();
+		X509Certificate eHealthCertificate = (X509Certificate) eHealthKeyStore
+				.getCertificate(alias);
+		PrivateKey eHealthPrivateKey = (PrivateKey) eHealthKeyStore.getKey(
+				alias, this.config.getEHealthPKCS12Password().toCharArray());
+
+		List<Attribute> attributes = new LinkedList<Attribute>();
+		attributes.add(new Attribute("urn:be:fgov:identification-namespace",
+				"urn:be:fgov:ehealth:1.0:certificateholder:person:ssin"));
+		attributes.add(new Attribute("urn:be:fgov:identification-namespace",
+				"urn:be:fgov:person:ssin"));
+
+		List<AttributeDesignator> attributeDesignators = new LinkedList<AttributeDesignator>();
+		attributeDesignators.add(new AttributeDesignator(
+				"urn:be:fgov:identification-namespace",
+				"urn:be:fgov:ehealth:1.0:certificateholder:person:ssin"));
+		attributeDesignators.add(new AttributeDesignator(
+				"urn:be:fgov:identification-namespace",
+				"urn:be:fgov:person:ssin"));
+		attributeDesignators.add(new AttributeDesignator(
+				"urn:be:fgov:certified-namespace:ehealth",
+				"urn:be:fgov:person:ssin:nurse:boolean"));
+
+		Element assertion = client.requestAssertion(authnCertificate,
+				authnPrivateKey, eHealthCertificate, eHealthPrivateKey,
+				attributes, attributeDesignators);
+
+		assertNotNull(assertion);
+
+		String assertionString = client.toString(assertion);
+
+		// eHealthBox: remove all messages.
+		EHealthBoxConsultationClient eHealthBoxClient = new EHealthBoxConsultationClient(
+				"https://services-acpt.ehealth.fgov.be/ehBoxConsultation/v3");
+		eHealthBoxClient.setCredentials(eHealthPrivateKey, assertionString);
+
+		GetMessageListResponseType messageList = eHealthBoxClient
+				.getMessagesList();
+		for (Message message : messageList.getMessage()) {
+			String messageId = message.getMessageId();
+			LOG.debug("message id: " + messageId);
+			eHealthBoxClient.deleteMessage(messageId);
+		}
+
+		// eHealthBox: publish
+		EHealthBoxPublicationClient publicationClient = new EHealthBoxPublicationClient(
+				"https://services-acpt.ehealth.fgov.be/ehBoxPublication/v3");
+
+		ObjectFactory objectFactory = new ObjectFactory();
+		PublicationMessageType publicationMessage = objectFactory
+				.createPublicationMessageType();
+		String publicationId = UUID.randomUUID().toString().substring(1, 13);
+		LOG.debug("publication id: " + publicationId);
+		publicationMessage.setPublicationId(publicationId);
+
+		DestinationContextType destinationContext = objectFactory
+				.createDestinationContextType();
+		publicationMessage.getDestinationContext().add(destinationContext);
+		destinationContext.setQuality("NURSE");
+		destinationContext.setType("INSS");
+		destinationContext.setId(getUserIdentifier(authnCertificate));
+
+		ContentContextType contentContext = objectFactory
+				.createContentContextType();
+		publicationMessage.setContentContext(contentContext);
+
+		PublicationContentType publicationContent = objectFactory
+				.createPublicationContentType();
+		contentContext.setContent(publicationContent);
+		PublicationDocumentType publicationDocument = objectFactory
+				.createPublicationDocumentType();
+		publicationContent.setDocument(publicationDocument);
+		publicationDocument.setTitle("test");
+		publicationDocument.setMimeType("text/plain");
+		publicationDocument.setDownloadFileName("test.txt");
+		byte[] data = "hello world".getBytes();
+		publicationDocument.setEncryptableTextContent(data);
+		publicationDocument.setEncryptableBinaryContent(null);
+		MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+		byte[] digest = messageDigest.digest(data);
+		publicationDocument.setDigest(Base64.encodeBase64String(digest));
+
+		ContentSpecificationType contentSpecification = objectFactory
+				.createContentSpecificationType();
+		contentContext.setContentSpecification(contentSpecification);
+		contentSpecification.setContentType("DOCUMENT");
+
+		publicationClient.setCredentials(eHealthPrivateKey, assertionString);
+		publicationClient.publish(publicationMessage);
+
+		// give eHealthBox some time.
+		Thread.sleep(1000 * 5);
+
+		LOG.debug("GET MESSAGES LIST");
+		messageList = eHealthBoxClient.getMessagesList();
+		for (Message message : messageList.getMessage()) {
+			String messageId = message.getMessageId();
+			LOG.debug("message id: " + messageId);
+			LOG.debug("GET FULL MESSAGE");
+			String request = "<ehbox:GetFullMessageRequest xmlns:ehbox=\"urn:be:fgov:ehealth:ehbox:consultation:protocol:v3\">"
+					+ "<Source>INBOX</Source>"
+					+ "<MessageId>"
+					+ messageId
+					+ "</MessageId>" + "</ehbox:GetFullMessageRequest>";
+			String response = eHealthBoxClient.invoke(request);
+			LOG.debug("RESPONSE: " + response);
+			JAXBContext consultationContext = JAXBContext
+					.newInstance(be.e_contract.mycarenet.ehbox.jaxb.consultation.protocol.ObjectFactory.class);
+			Unmarshaller consultationUnmarshaller = consultationContext
+					.createUnmarshaller();
+			Map<String, DataHandler> messageAttachments = eHealthBoxClient
+					.getMessageAttachments();
+			for (Map.Entry<String, DataHandler> messageAttachment : messageAttachments
+					.entrySet()) {
+				LOG.debug("message attachment id: "
+						+ messageAttachment.getKey());
+				LOG.debug("message data handler: "
+						+ messageAttachment.getValue());
+				DataHandler resultDataHandler = messageAttachment.getValue();
+				DataSource resultDataSource = resultDataHandler.getDataSource();
+				byte[] attachmentData = IOUtils.toByteArray(resultDataSource
+						.getInputStream());
+				LOG.debug("DataHandler.DataSource.getInputStream length: "
+						+ attachmentData.length);
 			}
 			consultationUnmarshaller
 					.setAttachmentUnmarshaller(new SOAPAttachmentUnmarshaller(
